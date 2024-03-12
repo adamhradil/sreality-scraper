dbe1f8faf2184a4194397dc6562667c238f5d286
Adam Hradil <hradilad@gmail.com>
2024-03-12 19:31:39 +0100

    add sreality api enums

--- sreality/spiders/sreality_spider.py
+++ sreality/spiders/sreality_spider.py
@@ -4,6 +4,78 @@ import json
 
 from sreality_scraper.sreality.items import SrealityItem
 
+class CategoryMainCb(Enum):
+    APARTMENT = 1
+    HOUSE = 2
+    LAND = 3
+    COMMERCIAL = 4
+    OTHER = 5
+
+
+class CategorySubCb(Enum):
+    NA = 1
+    ONE_PLUS_KK = 2
+    ONE_PLUS_ONE = 3
+    TWO_PLUS_KK = 4
+    TWO_PLUS_ONE = 5
+    THREE_PLUS_KK = 6
+    THREE_PLUS_ON = 7
+    FOUR_PLUS_KK = 8
+    FOUR_PLUS_ONE = 9
+    FIVE_PLUS_KK = 10
+    FIVE_PLUS_ONE = 11
+    SIX_AND_LARGER = 12
+    UNUSUAL = 16
+    ROOM = 47  # leased apartments only
+    # land
+    HOUSINGS = 19
+    COMMERCIAL = 18
+    FIELDS = 20
+    MEADOWS = 22
+    FORRESTS = 21
+    PONDS = 46
+    ORCHARD_VINEYARD = 48
+    GARDEN = 23
+    OTHER_LAND = 24
+    # commercial
+    OFFICES = 25
+    WAREHOUSES = 26
+    PRODUCTION = 27
+    RETAIL_SPACES = 28
+    ACCOMMODATIONS = 29
+    RESTAURANTS = 30
+    AGRICULTURE = 31
+    APARTMENT_BUILDING = 38
+    OTHER_COMMERCIAL = 32
+    DOCTORS_OFFICE = 56
+    APARTMENTS = 57
+    VIRTUAL_OFFICE = 49  # lease only
+
+    # other
+    GARAGE = 34
+    PARKING_PLACE = 52
+    MOBILE_HOME = 53
+    WINE_CELLAR = 50
+    ATTIC = 51
+    OTHER = 36
+
+
+# houses
+class RoomCountCb(Enum):
+    ONE_ROOM = 1
+    TWO_ROOMS = 2
+    THREE_ROOMS = 3
+    FOUR_ROOMS = 4
+    FIVE_ROOMS = 5
+    UNUSUAL = 6
+
+
+class CategoryTypeCb(Enum):
+    PRODEJ = 1
+    PRONAJEM = 2
+    DRAZBA = 3
+    PODIL = 4
+
 
 class SrealitySpider(scrapy.Spider):
     name = "sreality"
