7c26cfd3d04d552b1d78031a1501440a217a188c
Adam Hradil <adam.hradil@oracle.com>
2024-04-29 17:57:34 +0200

    formatting

--- sreality/spiders/sreality_spider.py
+++ sreality/spiders/sreality_spider.py
@@ -6,7 +6,6 @@ import scrapy  # type: ignore
 from sreality_scraper.sreality.items import SrealityItem
 
 
-
 class SrealityUrlBuilder:
     @staticmethod
     def map_category_main_cb(category_main_cbx):
@@ -15,7 +14,7 @@ class SrealityUrlBuilder:
             CategoryMainCb.HOUSE.value: "dum",
             CategoryMainCb.LAND.value: "pozemek",
             CategoryMainCb.COMMERCIAL.value: "komercni",
-            CategoryMainCb.OTHER.value: "ostatni"
+            CategoryMainCb.OTHER.value: "ostatni",
         }
         return mapping.get(category_main_cbx)
 
@@ -48,7 +47,7 @@ class SrealityUrlBuilder:
             CategoryTypeCb.PRONAJEM.value: "pronajem",
             CategoryTypeCb.PRODEJ.value: "prodej",
             CategoryTypeCb.DRAZBA.value: "drazba",
-            CategoryTypeCb.PODIL.value: "podil"
+            CategoryTypeCb.PODIL.value: "podil",
         }
         return mapping.get(category_type_cbx)
 
@@ -57,7 +56,7 @@ class SrealityUrlBuilder:
         mapping = {
             FurnishedCategory.YES.value: "vybaveny",
             FurnishedCategory.NO.value: "nevybaveny",
-            FurnishedCategory.PARTIAL.value: "castecne"
+            FurnishedCategory.PARTIAL.value: "castecne",
         }
         return mapping.get(furnished_category)
 
@@ -66,7 +65,7 @@ class SrealityUrlBuilder:
         mapping = {
             BuildingTypeCategory.PANEL.value: "panelova",
             BuildingTypeCategory.CIHLA.value: "cihlova",
-            BuildingTypeCategory.OSTATNI.value: "ostatni"
+            BuildingTypeCategory.OSTATNI.value: "ostatni",
         }
         return mapping.get(building_type)
 
@@ -190,14 +189,22 @@ class SrealitySpider(scrapy.Spider):
         }
 
         self.params = [
-            ("category_main_cb", params_mapping[spider_settings['estate_type']]),
-            ("category_type_cb", params_mapping[spider_settings['listing_type']]),
-            ("region", params_mapping[spider_settings['location']]['region']),
-            ("region_entity_id", params_mapping[spider_settings['location']]['region_entity_id']),
-            ("region_entity_type", params_mapping[spider_settings['location']]['region_entity_type']),
+            ("category_main_cb", params_mapping[spider_settings["estate_type"]]),
+            ("category_type_cb", params_mapping[spider_settings["listing_type"]]),
+            ("region", params_mapping[spider_settings["location"]]["region"]),
+            (
+                "region_entity_id",
+                params_mapping[spider_settings["location"]]["region_entity_id"],
+            ),
+            (
+                "region_entity_type",
+                params_mapping[spider_settings["location"]]["region_entity_type"],
+            ),
             ("per_page", "60"),
         ]
-        self.base_url = "https://www.sreality.cz/api/cs/v2/estates?" + urlencode(self.params)
+        self.base_url = "https://www.sreality.cz/api/cs/v2/estates?" + urlencode(
+            self.params
+        )
         self.per_page = 60  # Number of items per page
 
     def start_requests(self):
@@ -206,7 +213,7 @@ class SrealitySpider(scrapy.Spider):
 
     def parse(self, response):
         data = json.loads(response.text)
-        result_size = data.get('result_size')
+        result_size = data.get("result_size")
         total_pages = result_size // self.per_page + 2
 
         yield from self.parse_page(data)
@@ -216,15 +223,21 @@ class SrealitySpider(scrapy.Spider):
             yield scrapy.Request(url)
 
     def parse_page(self, data):
-        if data['_embedded']['estates']:
-            estates = [estate['_links']['self']['href'] for estate in data['_embedded']['estates'] if estate['region_tip'] == 0]
+        if data["_embedded"]["estates"]:
+            estates = [
+                estate["_links"]["self"]["href"]
+                for estate in data["_embedded"]["estates"]
+                if estate["region_tip"] == 0
+            ]
             for href in estates:
-                yield scrapy.Request(f"https://www.sreality.cz/api{href}", callback=self.parse_estate)
+                yield scrapy.Request(
+                    f"https://www.sreality.cz/api{href}", callback=self.parse_estate
+                )
 
     def get_first_or_none(self, l1: list, string: str):
-        filtered_dict = [item for item in l1 if string in item['name']]
+        filtered_dict = [item for item in l1 if string in item["name"]]
         if len(filtered_dict) > 0:
-            return filtered_dict[0].get('value')
+            return filtered_dict[0].get("value")
         return None
 
     def build_sreality_url(self, data):
@@ -232,7 +245,7 @@ class SrealitySpider(scrapy.Spider):
         category_sub_cbx = data.get("seo").get("category_sub_cb")
         category_type_cbx = data.get("seo").get("category_type_cb")
         locality = data.get("seo").get("locality")
-        hash_id = data.get('recommendations_data').get('hash_id')
+        hash_id = data.get("recommendations_data").get("hash_id")
 
         category_main_cbx = SrealityUrlBuilder.map_category_main_cb(category_main_cbx)
         category_sub_cbx = SrealityUrlBuilder.map_category_sub_cb(category_sub_cbx)
@@ -255,34 +268,54 @@ class SrealitySpider(scrapy.Spider):
     def parse_estate(self, response):
         sreality_item = SrealityItem()
         data = json.loads(response.text)
-        sreality_item["id"] = int(data.get('recommendations_data').get('hash_id'))
-        sreality_item["type"] = data.get('codeItems').get('building_type_search')
+        sreality_item["id"] = int(data.get("recommendations_data").get("hash_id"))
+        sreality_item["type"] = data.get("codeItems").get("building_type_search")
         sreality_item["url"] = self.build_sreality_url(data)
-        sreality_item["price"] = data.get('price_czk').get('value_raw')
-        sreality_item["service_fees"] = self.get_first_or_none(data.get('items'), "Poznámka k ceně")
+        sreality_item["price"] = data.get("price_czk").get("value_raw")
+        sreality_item["service_fees"] = self.get_first_or_none(
+            data.get("items"), "Poznámka k ceně"
+        )
         # security_deposit =
         sreality_item["address"] = data.get("locality").get("value")
-        sreality_item["address_accuracy"] = data.get('locality').get('accuracy')
-        sreality_item["description"] = data.get('text').get('value')
-        sreality_item["disposition"] = data.get('seo').get('category_sub_cb') if data.get('seo').get('category_main_cb') == 1 else self.convert_room_count(data.get('recommendations_data').get('room_count_cb'))
-        sreality_item["available_from"] = self.get_first_or_none(data.get('items'), "Datum nastěhování")
-        sreality_item["floor"] = self.get_first_or_none(data.get('items'), "Podlaží")
-        sreality_item["area"] = self.get_first_or_none(data.get('items'), "Užitná plocha")
-        sreality_item["furnished"] = data.get('recommendations_data').get('furnished')
-        sreality_item["status"] = self.get_first_or_none(data.get('items'), "Stav objektu")
-        sreality_item["ownership"] = data.get('codeItems').get('ownership')
-        sreality_item["penb"] = self.get_first_or_none(data.get('items'), "Energetická náročnost budovy")
+        sreality_item["address_accuracy"] = data.get("locality").get("accuracy")
+        sreality_item["description"] = data.get("text").get("value")
+        sreality_item["disposition"] = (
+            data.get("seo").get("category_sub_cb")
+            if data.get("seo").get("category_main_cb") == 1
+            else self.convert_room_count(
+                data.get("recommendations_data").get("room_count_cb")
+            )
+        )
+        sreality_item["available_from"] = self.get_first_or_none(
+            data.get("items"), "Datum nastěhování"
+        )
+        sreality_item["floor"] = self.get_first_or_none(data.get("items"), "Podlaží")
+        sreality_item["area"] = self.get_first_or_none(
+            data.get("items"), "Užitná plocha"
+        )
+        sreality_item["furnished"] = data.get("recommendations_data").get("furnished")
+        sreality_item["status"] = self.get_first_or_none(
+            data.get("items"), "Stav objektu"
+        )
+        sreality_item["ownership"] = data.get("codeItems").get("ownership")
+        sreality_item["penb"] = self.get_first_or_none(
+            data.get("items"), "Energetická náročnost budovy"
+        )
         # design =
-        sreality_item["balcony"] = data.get('recommendations_data').get('balcony')
-        sreality_item["cellar"] = data.get('recommendations_data').get('cellar')
-        sreality_item["garden"] = data.get('recommendations_data').get('garden_area')
-        sreality_item["terrace"] = data.get('recommendations_data').get('terrace')
-        sreality_item["elevator"] = data.get('recommendations_data').get('elevator')
-        sreality_item["parking"] = data.get('recommendations_data').get('parking_lots')
-        sreality_item["garage"] = data.get('recommendations_data').get('garage')
+        sreality_item["balcony"] = data.get("recommendations_data").get("balcony")
+        sreality_item["cellar"] = data.get("recommendations_data").get("cellar")
+        sreality_item["garden"] = data.get("recommendations_data").get("garden_area")
+        sreality_item["terrace"] = data.get("recommendations_data").get("terrace")
+        sreality_item["elevator"] = data.get("recommendations_data").get("elevator")
+        sreality_item["parking"] = data.get("recommendations_data").get("parking_lots")
+        sreality_item["garage"] = data.get("recommendations_data").get("garage")
         # pets =
-        sreality_item["loggie"] = data.get('recommendations_data').get('loggia')
-        sreality_item["gps_lat"] = data.get('recommendations_data').get('locality_gps_lat')
-        sreality_item["gps_lon"] = data.get('recommendations_data').get('locality_gps_lon')
+        sreality_item["loggie"] = data.get("recommendations_data").get("loggia")
+        sreality_item["gps_lat"] = data.get("recommendations_data").get(
+            "locality_gps_lat"
+        )
+        sreality_item["gps_lon"] = data.get("recommendations_data").get(
+            "locality_gps_lon"
+        )
 
         yield sreality_item
